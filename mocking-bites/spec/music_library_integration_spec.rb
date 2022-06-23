5370a360b7baceb3e91391cee2d34250c97596d9
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-06-23 15:15:56 +0100

    Crafting doubles: doubles_spec.rb - passed 5 tests

--- mocking-bites/spec/music_library_integration_spec.rb
+++ mocking-bites/spec/music_library_integration_spec.rb
@@ -7,7 +7,22 @@ describe "integration" do
       music = MusicLibrary.new
       track_1 = Track.new("title_1", "artist_1")
       music.add(track_1)
-      expect(music.all).to eq [track_1]
+      track_2 = Track.new("title_2", "artist_2")
+      music.add(track_2)
+      expect(music.all).to eq [track_1, track_2]
+    end
+  end
+
+  context "when searching tracks by keyword" do
+    it "returns the tracks containing this keyword" do
+      music = MusicLibrary.new
+      track_1 = Track.new("I'm in love_1", "Alex_1")
+      music.add(track_1)
+      track_2 = Track.new("Firebird_2", "John_2")
+      music.add(track_2)
+      track_3 = Track.new("Firebird_5", "Love_band")
+      music.add(track_3)
+      expect( music.search("love") ).to eq [track_1, track_3]
     end
   end
 end 
